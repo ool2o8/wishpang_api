46f8f2060e1fce22e1e1f3f8dd22fe63b0aa8e1a
Zoe <Zoe.kang@travelflan.com>
2022-07-07 22:47:11 +0900

    get password hashing, add requirements

--- foodiary/account/account/models.py
+++ foodiary/account/account/models.py
@@ -1,9 +1,9 @@
 from django.forms import CharField
 from django.utils import timezone
-from django.contrib.auth.models import AbstractUser, User
+from django.contrib.auth.models import AbstractUser, User, BaseUserManager
 from django.db import models
 
+class Profile(models.Model):
+	user=models.OneToOneField(User, on_delete=models.CASCADE)
+	nickname=models.TextField(max_length=50)
 
-class User(User):
-	username=User.username
-	password=User.password
